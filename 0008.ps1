b4ebdcfd556e577942bbab606e9dc9ceeaa00197
panelresources <153452859+panelresources@users.noreply.github.com>
2024-05-22 04:22:34 +0100

    Update 0008.ps1

--- 0008.ps1
+++ 0008.ps1
@@ -2,32 +2,41 @@
 Write-Host "Starting the installation script for Edbot"
 Write-Host "Git Clone the Edbot Repository"
 
+# Prompt the user for their GitHub Personal Access Token
 $credentials = Read-Host "Enter your GitHub Personal Access Token"
+
+# Specify the repository and target directory
 $repo = "panelresources/edbot"
 $targetDirectory = "$env:USERPROFILE\Documents\GitHub\edbot"
 
-New-Item -ItemType Directory -Path $targetDirectory
-# Check if the directory was created successfully
-if (Test-Path $targetDirectory) {
-    Write-Host "Directory created successfully at $targetDirectory"
-} else {
-    Write-Host "Failed to create the directory"
+# Create the target directory if it doesn't exist
+if (-not (Test-Path $targetDirectory)) {
+    New-Item -ItemType Directory -Path $targetDirectory | Out-Null
+    if (Test-Path $targetDirectory) {
+        Write-Host "Directory created successfully at $targetDirectory"
+    } else {
+        Write-Host "Failed to create the directory"
+        exit 1
+    }
 }
 
 # Construct the URL for the zipball (archive)
 $zipUrl = "https://api.github.com/repos/$repo/zipball/main"
 
+# Specify the path for the downloaded zip file
+$zipFilePath = Join-Path -Path $targetDirectory -ChildPath "repo.zip"
+
 # Download the zipball
-$zipFilePath = "$targetDirectory\repo.zip"
 Invoke-WebRequest -Uri $zipUrl -Headers @{ "Authorization" = "token $credentials" } -OutFile $zipFilePath
 
-# Extract the zip file
+# Extract the zip file to the target directory
 Expand-Archive -Path $zipFilePath -DestinationPath $targetDirectory
 
 # Delete the zip file
 Remove-Item -Path $zipFilePath
 
 Write-Host "Repository downloaded, extracted, and zip file deleted successfully!"
+
 
 
 
