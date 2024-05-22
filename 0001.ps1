de81207ff7bdb06d5a356c628e93ff1f28aeb5d2
panelresources <153452859+panelresources@users.noreply.github.com>
2024-05-22 01:25:52 +0100

    Update 0001.ps1

--- 0001.ps1
+++ 0001.ps1
@@ -10,8 +10,31 @@ Invoke-WebRequest -Uri "$repoUrl/archive/refs/heads/main.zip" -OutFile "plinqo.z
 # Extract the ZIP file
 Expand-Archive -Path "plinqo.zip" -DestinationPath "."
 
-# Rename the extracted folder
-Rename-Item -Path ".\plinqo-main" -NewName "plinqo"
 
-# Clean up the ZIP file
-Remove-Item -Path "plinqo.zip"
+# Download the Docker Desktop installer
+$installerUrl = "https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe"
+$installerPath = "$env:TEMP\DockerDesktopInstaller.exe"
+
+Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+
+# Install Docker Desktop silently
+Start-Process -FilePath $installerPath -ArgumentList "install --quiet" -Wait -NoNewWindow
+
+# Clean up the installer
+Remove-Item -Path $installerPath
+
+
+
+if (-not (Get-Command choco.exe -ErrorAction SilentlyContinue)) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+# List of packages to install
+$packages = 'git'
+
+# Install each package silently
+foreach ($package in $packages) {
+    choco install $package -y
+}
