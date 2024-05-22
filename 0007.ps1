8d103a4de9cc8d2be19f6ea00ddfc3d8ea81b5df
panelresources <153452859+panelresources@users.noreply.github.com>
2024-05-22 04:12:42 +0100

    Update 0007.ps1

--- 0007.ps1
+++ 0007.ps1
@@ -6,6 +6,14 @@ $credentials = Read-Host "Enter your GitHub Personal Access Token"
 $repo = "panelresources/edbot"
 $targetDirectory = "$env:USERPROFILE\Documents\GitHub\edbot"
 
+New-Item -ItemType Directory -Path $targetDirectory
+# Check if the directory was created successfully
+if (Test-Path $targetDirectory) {
+    Write-Host "Directory created successfully at $targetDirectory"
+} else {
+    Write-Host "Failed to create the directory"
+}
+
 # Construct the URL for the zipball (archive)
 $zipUrl = "https://api.github.com/repos/$repo/zipball/main"
 
