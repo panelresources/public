365e652db2f470c654b5018d729246c52a0ef268
panelresources <153452859+panelresources@users.noreply.github.com>
2024-05-22 03:21:28 +0100

    Add files via upload

---
+++ pre-install.ps1
@@ -0,0 +1,5 @@
+Write-Host "Pre-requisite Script, you may need to restart your PC"
+
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
