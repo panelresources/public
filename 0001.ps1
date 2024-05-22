bcf3eeefacd2a7cab59b018d7ed03dd5aabced80
panelresources <153452859+panelresources@users.noreply.github.com>
2024-05-22 01:11:05 +0100

    Update 0001.ps1

--- 0001.ps1
+++ 0001.ps1
@@ -1,2 +1,17 @@
-echo "REM installation script"
-echo "TEST"
+# Display some text
+Write-Host "Starting the installation script for Edbot"
+
+# Specify the GitHub repository URL
+$repoUrl = "https://github.com/ironmansoftware/plinqo"
+
+# Download the repository ZIP file
+Invoke-WebRequest -Uri "$repoUrl/archive/refs/heads/main.zip" -OutFile "plinqo.zip"
+
+# Extract the ZIP file
+Expand-Archive -Path "plinqo.zip" -DestinationPath "."
+
+# Rename the extracted folder
+Rename-Item -Path ".\plinqo-main" -NewName "plinqo"
+
+# Clean up the ZIP file
+Remove-Item -Path "plinqo.zip"
